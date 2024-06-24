4eea873cafc945290a9957a9c1d6b630932e9645
Martín Belda <32129742+mbdaso@users.noreply.github.com>
2024-06-24 21:01:48 +0100

    menu

---
+++ site/Components/Menu/Menu.cs
@@ -0,0 +1,22 @@
+﻿namespace site.Components.Menu
+{
+
+    public class Menu
+    {
+        public List<Category> Categories { get; set; } = new List<Category>();
+    }
+
+    public class Category
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<string> PriceCategories { get; set; } = new List<string>();
+        public List<Item> Items { get; set; } = new List<Item>();
+    }
+
+    public class Item
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Dictionary<string, string> Prices { get; set; } = new Dictionary<string, string>();
+    }
+}
